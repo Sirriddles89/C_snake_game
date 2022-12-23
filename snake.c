f591e9eb8db2796a0a0c7a57972c28992008600a
Oliver Motok <olivermotok@gmail.com>
2022-12-22 21:48:38 -0600

    Add setup function and modify boundary function setup seeds a random number generator to randomly place fruit within game area. Boundary function now prints a fruit('#') in a random location and a snake ('0') in the center of the game area

--- snake.c
+++ snake.c
@@ -1,27 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
 
 //game variables
-int i, j, game_over, score;
+int game_over, score;
 int length = 28;
 int width = 40;
+int X, Y, food_x, food_y, flag;
 
-//prototype for Boundary function
+//function prototypes
 void boundary();
+void setup();
 
 int main(void)
 {
-    //calling the boundary function draws a game window
-
+    
+    // call setup to generate fruit
+    setup();
+    //call boundary to draw game area around fruit
     boundary();
 }
 
 //boundary function - draws the space where gameplay will take place
 void boundary()
 {
-    for (i = 0; i <= length; i++)
+    for (int i = 0; i <= length; i++)
     {
-        for (j = 0; j <= width; j++)
+        for (int j = 0; j <= width; j++)
         {
             
             if (i == 0 || i == length)
@@ -34,9 +40,46 @@ void boundary()
             }
             else
             {
-                printf(" ");
+                if (i == X && j == Y)
+                {
+                    printf("0");
+                }
+                else if (i == food_x && j == food_y)
+                {
+                    printf("#");
+                }
+                else 
+                {
+                    printf(" ");
+                }
             }
         }
         printf("\n");
     }
+}
+
+//setup function - generates food within boundary
+void setup()
+{
+    //initialize random number generator
+    time_t t;
+    srand((unsigned) time(&t));
+    game_over = 0;
+
+    X = length / 2;
+    Y = width / 2;
+
+    state1:
+        food_x = rand() % 20;
+        if (food_x == 0)
+        {
+            goto state1;
+        }
+    state2:
+        food_y = rand() % 30;
+        if (food_y == 0)
+        {
+            goto state2;
+        }
+    score = 0;            
 }
