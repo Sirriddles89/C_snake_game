4507e4a37fea5f29cfe745a5798d5340f7411170
Oliver Motok <olivermotok@gmail.com>
2023-01-02 18:06:28 -0600

    Implement total rewrite using ncurses library With this commit the code has been rewritten from the ground up using the ncurses library to create a game window and implement non-buffered keyboard input. A lot of work remains, but the program now responds to keyboard input, printing a new '0' character each time the user presses an arrow key.

--- snake.c
+++ snake.c
@@ -1,155 +1,149 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <ncurses.h>
 #include <unistd.h>
+#include <stdbool.h>
+#include <string.h>
 #include <time.h>
-#include <termios.h>
-#include <unistd.h>
-#include <fcntl.h>
+#include <stdlib.h>
 
-//game variables
-int game_over, score;
-int length = 28;
-int width = 40;
-int X, Y, food_x, food_y, flag;
+#define DELAY 30000
 
-//function prototypes
-void boundary();
-void setup();
-void input();
-int kbhit();
+typedef struct s_head
+{
+   int location[2];
+   struct s_head* next;
+   int head;
+   int direction;
+   int next_x;
+}
+snake_head;
+
+typedef struct g_board
+{
+   int fruit[2];
+   int game_over;
+   int height; 
+   int width;
+}
+game_board;
+
+// Function prototypes
+void spawn_fruit(WINDOW *game);
+void input(WINDOW *game, int rows, int cols);
+
+game_board board = {.height = 53, .width = 63, .game_over = 0};
+snake_head head;
 
 int main(void)
 {
-    
-    // call setup to generate fruit
-    setup();
-    //call boundary to draw game area around fruit
-    boundary();
+   initscr();
+   noecho();
+   curs_set(FALSE);
+
+   //establish height/width for game area and center it
+   int rows, cols;
+   int start_y = (LINES - board.height) / 2; 
+   int start_x = (COLS - board.width) / 2; 
+   refresh();
+
+   // Create window for game area and put a border around it
+   WINDOW *game = newwin(board.height, board.width, start_y, start_x);
+   getmaxyx(game, rows, cols);
+   box(game, 0, 0); 
+   wrefresh(game);
+
+   // Call spawn_fruit function to spawn initial fruit
+   spawn_fruit(game); 
+
+   // Set initial coordinates for snake head and print
+   head.location[1] = (board.height / 2);
+   head.location[2] = (board.width / 2);
+   mvwprintw(game, head.location[1], head.location[2], "0");
+   wrefresh(game);
+
+   // Call input function to move snake
+   while(true)
+   {
+      input(game, rows, cols);
+      wrefresh(game);
+   }
+   
+
+   getchar();
+   endwin();
+   return 0;
 }
 
-//boundary function - draws the space where gameplay will take place
-void boundary()
+// Spawn fruit on game board
+void spawn_fruit(WINDOW *game)
 {
-    for (int i = 0; i <= length; i++)
-    {
-        for (int j = 0; j <= width; j++)
-        {
-            
-            if (i == 0 || i == length)
-            {
-                printf("*");
-            }
-            else if (i > 0 && j == 0 || i > 0 && j == width)
-            {
-                printf("*");
-            }
-            else
-            {
-                if (i == X && j == Y)
-                {
-                    printf("0");
-                }
-                else if (i == food_x && j == food_y)
-                {
-                    printf("#");
-                }
-                else 
-                {
-                    printf(" ");
-                }
-            }
-        }
-        printf("\n");
-    }
-}
-
-//setup function - generates food within boundary
-void setup()
-{
-    //initialize random number generator
+   //initialize random number generator
     time_t t;
     srand((unsigned) time(&t));
-    game_over = 0;
-
-    X = length / 2;
-    Y = width / 2;
 
     state1:
-        food_x = rand() % 20;
-        if (food_x == 0)
+        board.fruit[0] = rand() % 50;
+        if (board.fruit[0] == 0)
         {
             goto state1;
         }
     state2:
-        food_y = rand() % 30;
-        if (food_y == 0)
+        board.fruit[1] = rand() % 60;
+        if (board.fruit[1] == 0)
         {
             goto state2;
         }
-    score = 0;            
+   
+   mvwprintw(game, board.fruit[0], board.fruit[1], "#");
+   wrefresh(game);
 }
 
-//function to control movement
-void input()
+void input(WINDOW *game, int rows, int cols)
 {
-    char direction;
-    if (kbhit())
-    {
-        switch(getchar())
+   keypad(game, TRUE);
+   int ch;
+   int y = head.location[1];
+   int x = head.location[2];
+   cbreak();
+   while((ch = wgetch(game)) != KEY_END)
+   {
+      
+      switch (ch)
         {
-            case 'a':
-            {
-                flag = 1;
+            case KEY_UP:
+                if (y > 0)
+                {
+                    --y;
+                    break;
+                }
+            
+            case KEY_DOWN:
+                if (y < (rows - 2))
+                {
+                    ++y;
+                    break;
+                }
+            
+            case KEY_LEFT:
+                if (x > 0)
+                {
+                    --x;
+                    break;
+                }
+            
+            case KEY_RIGHT:
+                if (x < (cols - 2))
+                {
+                    ++x;
+                    break;
+                }
+
+            case KEY_HOME:
+                x = 0;
+                y = 0;
                 break;
-            }
-            case 's':
-            {
-                flag = 2;
-                break;             
-            }
-            case 'd': 
-            {
-                flag = 3;
-                break;
-            }
-            case 'w':
-            {
-                flag = 4;
-                break;
-            }
-            case 'q':
-            {
-                game_over = 1;
-                break;
-            }
         }
-    }
-}
 
-//kbhit function (taken from https://stackoverflow.com/questions/22166074/is-there-a-way-to-detect-if-a-key-has-been-pressed)
-int kbhit()
-{
-  struct termios oldt, newt;
-  int ch;
-  int oldf;
-
-  tcgetattr(STDIN_FILENO, &oldt);
-  newt = oldt;
-  newt.c_lflag &= ~(ICANON | ECHO);
-  tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-  oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
-  fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
-
-  ch = getchar();
-
-  tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-  fcntl(STDIN_FILENO, F_SETFL, oldf);
-
-  if(ch != EOF)
-  {
-    ungetc(ch, stdin);
-    return 1;
-  }
-
-  return 0;
+      mvwprintw(game, y, x, "0");
+      wrefresh(game);
+   }
 }
